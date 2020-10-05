ed647f3fa8472b4051804c99e4e7db03251b33e0
photongupta <58028408+photongupta@users.noreply.github.com>
2020-10-05 13:27:05 +0000

    Backend: modified deploy.sh

---
+++ public/build/precache-manifest.b2da8ae3a141293494667536975b1aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f67ebf09d5636250d0c23458ed7113f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30492655475f036e5693",
+    "url": "/static/css/main.686d698e.chunk.css"
+  },
+  {
+    "revision": "5d4e4fb97223ad0e528b",
+    "url": "/static/js/2.a6636ceb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6636ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30492655475f036e5693",
+    "url": "/static/js/main.55017316.chunk.js"
+  },
+  {
+    "revision": "c0fa6ca7d1f064a94e45",
+    "url": "/static/js/runtime-main.2e8bd11b.js"
+  }
+]);
